24110bd0ab0d8d2f63a7c145c68184972b686da3
Bruno do Amaral <bruno.moraes.zyb@gmail.com>
2026-05-08 21:51:05 -0300

    fix

--- brokenlibrary/public/main.js
+++ brokenlibrary/public/main.js
@@ -1,3 +1,4 @@
 alert('john')
 
-document.getElementById('main').innerHTML = '<p>Hello Worldddd</p>'
+document.getElementById('main').innerHTML = '<p>Site made as a Library to read, add, edit and delete books, as well as to manage users, and rate them with comments.</p>'
+
--- brokenlibrary/server.js
+++ brokenlibrary/server.js
@@ -1,8 +1,21 @@
 const express = require('express')
-
-console.log("Hello World")
+const path = require('path')
 
 const app = express()
+
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'))
+})
+
+app.get('/books', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'books.html'))
+})
+
+app.get('/users', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'users.html'))
+})
 
 app.listen(3000, () => {
     console.log("Server started on port 3000")
